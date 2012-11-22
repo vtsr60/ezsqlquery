c40b6c7e78665baf13b7d2a93a1c512a24fbd6a2
Vivakran, Thanga Senthil Rajan(raj) <vtsr60@gmail.com>
2012-11-22 18:43:05 +0530

    Initial extension structure

---
+++ ezinfo.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package nxcString
+ * @class   nxc_stringInfo
+ * @author  Serhey Dolgushev <serhey.dolgushev@nxc.no>
+ * @date    08 Sep 2011
+ **/
+
+class ezsqlqueryInfo {
+	public static function info() {
+		return array(
+			'Name'      => 'SQL Query Datatype',
+			'Version'   => '1.0',
+			'Author'    => 'Vivakaran, Thanks Senthil Rajan(Raj)',
+			'Copyright' => 'Copyright &copy; 2012'
+		);
+	}
+}
+?>
